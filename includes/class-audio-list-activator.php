a6b982029ca166d0b3d9ac28f8d6e2435d54ebf6
Jack <xjlin0@gmail.com>
2024-05-17 09:32:50 -0700

    add create table query for plugin activation

--- includes/class-audio-list-activator.php
+++ includes/class-audio-list-activator.php
@@ -30,7 +30,34 @@ class Audio_List_Activator {
 	 * @since    1.0.0
 	 */
 	public static function activate() {
+      global $wpdb;
+      $table_name = $wpdb->prefix . 'audio_list';
+      $charset_collate = $wpdb->get_charset_collate();
+      $charset_collate_array = explode('COLLATE ', $charset_collate);
+      $collate = end($charset_collate_array);
 
+      $sql = "CREATE TABLE IF NOT EXISTS $table_name (
+        `id` int(11) NOT NULL AUTO_INCREMENT,
+        `sermondate` date DEFAULT NULL,
+        `speaker` varchar(255) COLLATE $collate DEFAULT NULL,
+        `topic` varchar(255) COLLATE $collate DEFAULT NULL,
+        `section` varchar(255) COLLATE $collate DEFAULT NULL,
+        `location` varchar(255) COLLATE $collate DEFAULT NULL,
+        `type` varchar(45) COLLATE $collate NOT NULL DEFAULT '主日崇拜',
+        `remark` varchar(255) COLLATE $collate DEFAULT NULL,
+        `audiofile` varchar(255) COLLATE $collate DEFAULT NULL,
+        `bibleID` int(11) DEFAULT '0',
+        `updatedBy` varchar(255) COLLATE $collate DEFAULT NULL,
+        `updatedTime` datetime DEFAULT NULL,
+        `activeFlag` varchar(45) COLLATE $collate DEFAULT 'Active',
+        PRIMARY KEY (`id`),
+        KEY `idx_sermondate` (`sermondate`),
+        KEY `idx_type` (`type`),
+        KEY `idx_activeFlag` (`activeFlag`)
+      ) $charset_collate;";
+
+      require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+      dbDelta( $sql );
 	}
 
 }
